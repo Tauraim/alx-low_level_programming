27a8bd7643bf2d639c23d109c55200365b367c9f
Tauraim <tmuzembe@outlook.com>
2023-09-07 15:04:57 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main- runs the file output
+ *
+ * return returds a zero if the function is successful
+ * 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	
+}
