6f85258edb70d600d97ae37cce4a5498ed4670f9
Tauraim <tmuzembe@outlook.com>
2023-09-07 15:13:40 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ *
+ * main- will print out function output 
+ *
+ * return will return zero if run success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
