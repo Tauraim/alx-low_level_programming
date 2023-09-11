08718eb25404f010f864143f39ae2e1cc9a98a52
Tauraim <tmuzembe@outlook.com>
2023-09-11 06:48:24 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,16 +2,18 @@
 
 /**
  * main - entry point fuction core
- * Description: 'print all possible combinations of a single-digit numbers
+ * Description: 'print all possible combinations of a single-digit numbers'
  * Return; always return zero if successful
  */
 int main(void)
 {
 	int n;
-
+	/*Using the ACSII table to map the range of requiired numbers*/
+	
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
+	/*Removing a comma from the last digit to be printed*/
 		if (n == 57)
 		{
 			continue;
