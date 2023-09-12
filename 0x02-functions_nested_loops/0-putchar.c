05c48cb2e29890e973155fbfdcef90616c314465
Tauraim <tmuzembe@outlook.com>
2023-09-12 02:09:43 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point function core
+ * Description: 'write and print out _putchar'
+ * Return: function should always return zero once successful
+ */
+int main(void)
+{
+	printf("_putchar\n");
+}
