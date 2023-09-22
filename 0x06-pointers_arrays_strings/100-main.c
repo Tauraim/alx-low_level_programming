aa80d1d4f75ce1f74a8fd9a8ee792f9a82268f9c
Tauraim <tmuzembe@outlook.com>
2023-09-21 19:34:45 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "100-main.c"
+
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif
