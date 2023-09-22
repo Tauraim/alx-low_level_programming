86177a2959c62545ca6169d72212679612bd0f94
Tauraim <tmuzembe@outlook.com>
2023-09-21 19:47:52 -0700

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "100-main.c"
 
+int putchar(int char)
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_number(int n);
+int putchar(int char);
+
+#endif
