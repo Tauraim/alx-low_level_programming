cdd246b6fd103ddf428511434253c4042a5318d0
Tauraim <tmuzembe@outlook.com>
2023-10-04 15:13:14 -0700

    Static Libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,11 @@
-
+#include <unistd.h>
+/**
+ * putchar - write character inputed
+ * @c: Character print out
+ * Return: return one if run success
+ * On error, on return the -one is returned as for the error.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
